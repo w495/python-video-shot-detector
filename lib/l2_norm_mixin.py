14e576d219f9daaa93673e3e9f7dfbe49aebdb3c
w495 <w495@yandex-team.ru>
2015-06-14 21:08:13 +0300

    add new norms

---
+++ lib/l2_norm_mixin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import
+
+import numpy as np
+
+from scipy.linalg import norm
+
+from .base_compare_mixin import BaseCompareMixin
+
+class L2NormMixin(BaseCompareMixin):
+
+    def get_difference(self, curr_features, other_features):
+        curr_vector =  np.array(curr_features)
+        other_vector =  np.array(other_features)
+        diff_vector = np.abs(curr_vector - other_vector)
+        res = norm(diff_vector.ravel(), 2)
+        mean_res = 1.0 * res  / (diff_vector.size * self.get_colour_size())
+        return mean_res
+

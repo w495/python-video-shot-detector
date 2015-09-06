f432239cdb1e0ee5405993edcfa951362e57f14f
w495 <w495@yandex-team.ru>
2015-09-07 01:57:36 +0300

    refactor BaseFrameHandler

--- shot_detector/handlers/base_frame_handler.py
+++ shot_detector/handlers/base_frame_handler.py
@@ -31,19 +31,40 @@ class BaseFrameHandler(BaseHandler):
     '''
     
     def handle_frame(self, frame, video_state = None, *args, **kwargs):
-        frame, video_state = self.select_frame(frame, video_state, *args, **kwargs)
+        frame, video_state = self.select_frame(
+            frame, 
+            video_state, 
+            *args, 
+            **kwargs
+        )
         if(frame):
-            video_state = self.handle_selected_frame(frame, video_state, *args, **kwargs)
+            video_state = self.handle_selected_frame(
+                frame, 
+                video_state, 
+                *args, 
+                **kwargs
+            )
         return video_state
 
     def handle_selected_frame(self, frame, video_state = None, *args, **kwargs):
-        features, video_state = self.extract_features(frame, video_state, *args, **kwargs)
-        raw_point = self.build_point_state(
-            features = features,
-            frame    = frame,
-            time = Second(frame.time)
+        features, video_state = self.extract_features(
+            frame, 
+            video_state, 
+            *args, 
+            **kwargs
         )
-        video_state = self.handle_point(raw_point, video_state, *args, **kwargs)
+        if(features):
+            raw_point = self.build_point_state(
+                features = features,
+                frame    = frame,
+                time = Second(frame.time)
+            )
+            video_state = self.handle_point(
+                raw_point, 
+                video_state, 
+                *args, 
+                **kwargs
+            )
         return video_state
 
     def build_point_state(self, *args, **kwargs):

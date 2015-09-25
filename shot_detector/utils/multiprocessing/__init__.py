4974c4eca4ba91caad2c9a1c9ddd8b2240c13d89
w495 <w495@yandex-team.ru>
2015-09-25 03:41:45 +0300

    add multiprocessing module

---
+++ shot_detector/utils/multiprocessing/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+from .base_point_state import BasePointState
+from .base_sliding_window_state import BaseSlidingWindowState
+from .base_video_state import BaseVideoState
+from .obj_dict import ObjDict
+from .second import Second
+from .smart_dict import SmartDict
+
+

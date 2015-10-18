9c6538263fdf07573fe7ac6da85aebffa730fe4f
w495 <w495@yandex-team.ru>
2015-10-18 22:53:48 +0300

    experiments with zcsore zero

--- shot_detector/test_detector.py
+++ shot_detector/test_detector.py
@@ -9,6 +9,8 @@ import time
 from .detectors import CommonDetector
 from .features.extractors import ImageBased
 from .features.extractors import VectorBased
+from .features.extractors.features.histogram import Histogram
+
 from .features.extractors.colours import RgbExtractor, LumaExtractor
 from .features.filters import BaseFilter
 from .selectors.event import BaseEventSelector
@@ -17,39 +19,11 @@ from .selectors.point import BasePointSelector
 from .settings import start_logging
 
 
-# from .lib.base_vector_mixin import BaseVectorMixin
-# from .lib.base_image_mixin import BaseImageMixin
-# from .lib.rgb_colour_mixin import RgbColourMixin
-# from .lib.gray_colour_mixin import GrayColourMixin
-# from .lib.histogram_mixin import HistogramMixin
-# from .lib.intersect_mixin import IntersectMixin
-# from .lib.sad_mixin import SadMixin
-# from .lib.adv_mixin import AdvMixin
-# from .lib.zero_norm_mixin import ZeroNormMixin
-# from .lib.l2_norm_mixin import L2NormMixin
-# from .lib.threshold_mixin import ThresholdMixin
-# from .lib.adaptive_threshold_mixin import AdaptiveThresholdMixin
-# from .lib.grad_adaptive_threshold_mixin import GradAdaptiveThresholdMixin
-# from .lib.fast_adaptive_threshold_mixin import FastAdaptiveThresholdMixin
-# from .lib.optical_flow_mixin import OpticalFlowMixin
-# from .lib.normalize_mixin import NormalizeMixin
-# class HistIntersectThresholdMixin(HistogramMixin, AdaptiveThresholdMixin,
-                                  # IntersectMixin, RgbColourMixin):
-    # THRESOLD = 0.8
-# class SadThresholdMixin(AdaptiveThresholdMixin, SadMixin, GrayColourMixin):
-    # THRESOLD = 0.18
-    # pass
-# class L2OpticalFlowMixin(AdaptiveThresholdMixin, OpticalFlowMixin, L2NormMixin, GrayColourMixin):
-    # pass
-# class SimpleImageDetector(SadThresholdMixin, BaseImageMixin, BaseDetector):
-    # pass
-# class SimpleVectorDetector(SadThresholdMixin, BaseVectorMixin, BaseDetector):
-    # pass
-# from shot_detector.handlers import BaseVideoHandler, BaseEventHandler, ParallelHandler
 class SimpleDetector(
     BaseEventSelector,
     # AdaptiveThresholdFilter,
     # LumaExtractor,
+    Histogram,
     VectorBased,
     CommonDetector,
 ):

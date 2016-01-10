f5395b9f2d14caf9f4313aa85b5e5af45af7a520
w-495 <w@w-495.ru>
2016-01-10 05:53:02 +0300

    fix SlidingWindow

--- shot_detector/utils/collections/sliding_window.py
+++ shot_detector/utils/collections/sliding_window.py
@@ -2,38 +2,241 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from collections import deque
-from itertools import islice
-from itertools import tee
-
-WINDOW_SIZE = 200
-FLUSH_LIMIT = -1
+import collections
+import types
+import itertools
 
 
-class SlidingWindow(deque):
+class SlidingWindow(collections.deque):
 
-    # @classmethod
-    # def sliding_windows2(cls, sequence, window_size=2, overlap_size=2):
-    #     sw_seq = SlidingWindow.sliding_windows(sequence, window_size)
-    #
-    #     for sw in sw_seq:
-    #         for item in sequence:
-    #
-
-
+    DEFAULT_WINDOW_SIZE = 2
 
     @classmethod
-    def sliding_windows(cls, sequence, window_size=2, overlap_size=None, yield_tail=True):
+    def sliding_windows(cls, sequence, window_size=None, overlap_size=None, yield_tail=None, strict_window=None):
         """
-        
-        :param sequence: 
-        :param window_size: 
-        :return: 
+        Generates a sequence of sliding windows over the `sequence` (second) parameter.
+        It expects that sequence is `iterable` and returns a `generator object`.
+        By default the size of the each window is 2,
+        but you can specify it with `window_size` (third) parameter.
+        Each window implements `collections.deque` interface.
+
+        How can you use it?
+        Let define initial sequence and function for uncovering generator content to tuple list.
+
+            >>> sequence = xrange(8)
+            >>>
+            >>> tuple(sequence)
+            (0, 1, 2, 3, 4, 5, 6, 7)
+            >>>
+            >>> def sliding_windows(*args, **kwargs):
+            ...     return list(tuple(sw) for sw in SlidingWindow.sliding_windows(*args, **kwargs))
+            ...
+            >>>
+
+        Default behaviour: Number of input itens equals to number of output ones.
+        This is not right sliding windows, but it is very usefull for many applications.
+
+            >>> soft_sw_list_2 = sliding_windows(sequence, window_size=2)
+            >>> soft_sw_list_2
+            [(0,), (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7)]
+            >>>
+            >>> len(sequence) == len(soft_sw_list_2)
+            True
+            >>>
+            >>> soft_sw_list_3 = sliding_windows(sequence, window_size=3)
+            >>> soft_sw_list_3
+            [(0,), (0, 1), (0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7)]
+            >>>
+            >>> len(sequence) == len(soft_sw_list_3)
+            True
+            >>>
+            >>> soft_sw_list_4 = sliding_windows(sequence, window_size=4)
+            >>> soft_sw_list_4
+            [(0,), (0, 1), (0, 1, 2), (0, 1, 2, 3), (1, 2, 3, 4), (2, 3, 4, 5), (3, 4, 5, 6), (4, 5, 6, 7)]
+            >>>
+            >>> len(sequence) == len(soft_sw_list_4)
+            True
+            >>>
+
+        It implements more accurate behaviour with `strict_window` parametr.
+
+            >>> strict_sw_list_2 = sliding_windows(
+            ...     sequence,
+            ...     window_size=2,
+            ...     strict_window=True
+            ... )
+            ...
+            >>> strict_sw_list_2
+            [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7)]
+            >>>
+            >>> len(sequence)
+            8
+            >>> len(strict_sw_list_2)
+            7
+            >>>
+            >>> strict_sw_list_3 = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     strict_window=True
+            ... )
+            >>> strict_sw_list_3
+            [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7)]
+            >>>
+            >>> len(sequence)
+            8
+            >>> len(strict_sw_list_3)
+            6
+            >>>
+            >>> strict_sw_list_4 = sliding_windows(
+            ...     sequence,
+            ...     window_size=4,
+            ...     strict_window=True
+            ... )
+            >>> strict_sw_list_4
+            [(0, 1, 2, 3), (1, 2, 3, 4), (2, 3, 4, 5), (3, 4, 5, 6), (4, 5, 6, 7)]
+            >>>
+            >>> len(sequence)
+            8
+            >>> len(strict_sw_list_4)
+            5
+            >>>
+
+        Also you can overlap windows.
+        By default windows overlapped with (window_size - 1) elements.
+
+            >>> strict_lapped_default = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     strict_window=True
+            ... )
+            ...
+            >>> strict_lapped_default
+            [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7)]
+            >>>
+            >>> strict_lapped_3_2 = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=2,
+            ...     strict_window=True
+            ... )
+            ...
+            >>> strict_lapped_3_2
+            [(0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7)]
+            >>>
+            >>> strict_lapped_3_1 = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=1,
+            ...     strict_window=True
+            ... )
+            ...
+            >>> strict_lapped_3_1
+            [(0, 1, 2), (2, 3, 4), (4, 5, 6)]
+            >>>
+
+        The same you can do with «soft» sliding windows.
+        Note that overlapping do not work on the incomplete windows
+        (at the start of window sequence).
+
+            >>> soft_lapped_3_1 = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=1,
+            ...     strict_window=False
+            ... )
+            ...
+            >>> soft_lapped_3_1
+            [(0,), (0, 1), (0, 1, 2), (2, 3, 4), (4, 5, 6)]
+            >>>
+
+        Look at the end of the `soft_lapped_3_1`. There is no `7`.
+        It has happened because 7 — the last member of initial sequence
+        and it do not fit with overlapping scheme.
+        For this case you can use `yield_tail` parameter.
+
+            >>> strict_lapped_3_1_tail = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=1,
+            ...     yield_tail=True,
+            ...     strict_window=True
+            ... )
+            ...
+            >>> strict_lapped_3_1_tail
+            [(0, 1, 2), (2, 3, 4), (4, 5, 6), (5, 6, 7)]
+            >>>
+
+        The same you can do with «soft» sliding windows.
+
+            >>> strict_lapped_3_1_tail = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=1,
+            ...     yield_tail=True,
+            ...     strict_window=False
+            ... )
+            ...
+            >>> strict_lapped_3_1_tail
+            [(0,), (0, 1), (0, 1, 2), (2, 3, 4), (4, 5, 6), (5, 6, 7)]
+            >>>
+
+        It is quite difficult to understand the logic of the last example
+        looking only on the result.
+
+        For fool-tolerance you cannot set `window_size` and `overlap_size` to the same number,
+        because it doesn't make sense.
+
+            >>> strict_lapped_3_2 = sliding_windows(
+            ...     sequence,
+            ...     window_size=3,
+            ...     overlap_size=3,
+            ... )
+            ...
+            Traceback (most recent call last):
+                ...
+            AssertionError: it does not make sense: overlap each window with `window_size`
+            >>>
+
+        In this case you should get:
+            [(0,), (0, 1), (0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2), ... an infinite number of times ... ]
+        You can achieve this much more simpler without sliding windows =).
+        Also you cannot set window_size less then null:
+
+            >>> strict_lapped_3_2 = sliding_windows(
+            ...     sequence,
+            ...     window_size=0,
+            ... )
+            ...
+            Traceback (most recent call last):
+                ...
+            AssertionError: it does not make sense: window with zero size
+            >>>
+
+        To sum up all examples, this is formal specification
+        :param collections.Iterable sequence:
+            initial sequence of any element.
+        :param integer window_size:
+            size of each sliding window
+        :param integer overlap_size: number
+        :param boolean yield_tail:
+        :param boolean strict_window:
+
+        :returns: <generator object sliding_windows at ... >
+            Do not forget about it. If you want to use the result of this functions several times
+            you should apply itertools.tee or convert this generator to concrete objects.
 
         """
-        assert window_size > 0
-        #assert overlap_size < window_size
+        assert isinstance(sequence, collections.Iterable)
+        assert isinstance(window_size, (int, long, types.NoneType))
+        assert isinstance(overlap_size, (int, long, types.NoneType))
+        assert isinstance(yield_tail, (bool, types.NoneType))
+        assert isinstance(strict_window, (bool, types.NoneType))
 
+        assert window_size > 0, 'it does not make sense: window with zero size'
+        assert overlap_size != window_size, 'it does not make sense: overlap each window with `window_size`'
+
+        if window_size is None:
+            window_size = cls.DEFAULT_WINDOW_SIZE
 
         if overlap_size is None:
             overlap_size = window_size - 1
@@ -43,74 +246,28 @@ class SlidingWindow(deque):
         yield_condition = None
         for index, item in enumerate(sequence):
             append(item)
-            print ('index = ', index,  win)
-            yield_condition = (index >= (window_size - 1) and not index % (window_size - overlap_size))
+            shift = window_size - overlap_size
+            yield_condition = (index <= (window_size - 1) or not index % shift)
+            if strict_window:
+                yield_condition = (index >= (window_size - 1) and not index % shift)
             if yield_condition:
-                print ('yield', list(win), yield_condition)
                 yield win
         else:
             if yield_tail and not yield_condition:
                 yield win
 
-
-    @classmethod
-    def tuple_seq(cls, sw_seq):
-        for sw in sw_seq:
-            yield tuple(sw)
-
-    @classmethod
-    def tuple(cls, sw_seq):
-        return tuple(cls.tuple_seq(sw_seq))
-
-
-    @staticmethod
-    def groups(sequence, window_size=2, reuse=1):
-
-        if reuse == 1:
-            sequence1, sequence2 = tee(sequence)
-            while True:
-                win = SlidingWindow(islice(sequence1, window_size), maxlen=window_size)
-                win.number = 0
-                if not len(win):
-                    break
-
-                yield win
-
-        if reuse == 2:
-            win = SlidingWindow([], maxlen=window_size)
-            append = win.append
-            win.number = 0
-
-            while True:
-                sl = islice(sequence, window_size)
-                s1, s2 = tee(sl)
-                for item in s1:
-                    append(item)
-                for _ in s2:
-                    yield win
-                else:
-                    break
-
-        if reuse == 3:
-            win = SlidingWindow([], maxlen=window_size)
-            win.number = 0
-
-            append = win.append
-            for item in sequence:
-                append(item)
-                yield win
-
     def __getitem__(self, index):
         if isinstance(index, slice):
-            return type(self)(islice(self, index.start, index.stop, index.step))
+            return type(self)(itertools.islice(self, index.start, index.stop, index.step))
         return super(SlidingWindow, self).__getitem__(index)
+
 
 def test():
     sequence = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-    sw = SlidingWindow.sliding_windows(sequence,1, 10)
-    print (SlidingWindow.tuple(sw))
-
-
+    sw_seq = SlidingWindow.sliding_windows(sequence,3)
+    print (sw_seq)
+    # sw = SlidingWindow.sliding_windows(sequence,2)
+    print (repr( sw_seq ))
 
 
 if __name__ == '__main__':

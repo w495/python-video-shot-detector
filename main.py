0fcf3f0e5ed5e90966096fb25a97195ff2e84c80
w-495 <w@w-495.ru>
2016-08-08 09:33:48 +0300

    fix main

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 
 from shot_detector.main_detector import SimpleDetector
 
-DEFAULT_FILE_NAME = '/run/media/w495/A2CAE41FCAE3ED8B/home/w495/Videos/Djadja_Stepa Milicioner_96.lw.und.mp4'
+DEFAULT_FILE_NAME = '/home/video/file.mp4'
 
 if __name__ == '__main__':
     detector = SimpleDetector()
-
     detector.detect(DEFAULT_FILE_NAME)

87bbe671a4c8a3e85ed38b45d123ac95ea6505e2
w-495 <w@w-495.ru>
2017-05-04 20:28:26 +0300

    add doc for FfmpegLikeEventPlotter

--- shot_detector/plotters/event/threshold/static/sad_ffmpeg_event_plotter.py
+++ shot_detector/plotters/event/threshold/static/sad_ffmpeg_event_plotter.py
@@ -1,5 +1,9 @@
 # -*- coding: utf8 -*-
 """
+    The illustration different types of video-filters:
+        * SadEventPlotter — sum of absolute difference filter
+        * FfmpegLikeEventPlotter — FFMpeg-like shot detection filter.
+    
     This is part of shot detector.
     Produced by w495 at 2017.05.04 04:18:27
 """
@@ -19,17 +23,30 @@ from .sad_event_plotter import SadEventPlotter
 
 class SadFfmpegEventPlotter(SadEventPlotter, FfmpegLikeEventPlotter):
     """
-        ...
+        Plotter for difference-based filters.
+        
+        It uses:
+            * SadEventPlotter — sum of absolute difference filter;
+            * FfmpegLikeEventPlotter — FFMpeg-like shot filter.
+        
     """
     __logger = logging.getLogger(__name__)
 
     @log_method_call_with(logging.WARN)
     def seq_filters(self):
         """
-        Returns the sequence of dict in which options of each chart
-        are described.
+            Chart options for Sad-filter and FFMpeg-kike illustration.
+            
+            :returns: filter chart options to describe filters.
+            :rtype: list of FilterDescription
         """
+
+        # Sum of absolute difference chat options.
         simple = SadEventPlotter.seq_filters(self)
+
+        # FFMpeg-like shot detection chat options.
         ffmpeg_like = FfmpegLikeEventPlotter.seq_filters(self)
+
+        # Deduplicate options union for each option set.
         result = unique(simple + ffmpeg_like)
         return result

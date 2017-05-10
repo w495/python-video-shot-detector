fb41d8347539663a14d029b18a95c2f4f32031a2
w-495 <w@w-495.ru>
2017-05-10 05:52:57 +0300

    refactor filter structure

--- shot_detector/filters/__init__.py
+++ shot_detector/filters/__init__.py
@@ -9,25 +9,28 @@ from __future__ import absolute_import, division, print_function
 
 from .filter import Filter
 
-from .atan_filter import AtanFilter
-from .bound_filter import BoundFilter
-from .colour_filter import ColourFilter
 from .condition_filter import ConditionFilter
-from .dct_filter import DCTFilter
 from .delay_filter import DelayFilter
-from .dht_filter import DHTFilter
-from .exp_filter import ExpFilter
-from .factor_filter import FactorFilter
-from .floor_filter import FloorFilter
-from .log_filter import LogFilter
-from .modulus_filter import ModulusFilter
-from .norm_filter import NormFilter
-from .otsu_filter import OtsuFilter
-from .sign_angle_diff_1d_filter import SignAngleDiff1DFilter
-from .sign_angle_diff_2d_filter import SignAngleDiff2DFilter
-from .sign_change_filter import SignChangeFilter
-from .slice_filter import SliceFilter
-from .sliding_window_filters import (
+
+from .common import (
+    AtanFilter,
+    BoundFilter,
+    ColourFilter,
+    DCTFilter,
+    DHTFilter,
+    ExpFilter,
+    FactorFilter,
+    FloorFilter,
+    LogFilter,
+    ModulusFilter,
+    NormFilter,
+    OtsuFilter,
+    SignAngleDiff1DFilter,
+    SignAngleDiff2DFilter,
+    SignChangeFilter,
+)
+
+from .sliding_window import (
     AlphaBetaSWFilter,
     BaseCombinationSWFilter,
     DetrendSWFilter,
---
+++ shot_detector/filters/common/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+"""
+    Filter collection
+"""
+
+from __future__ import absolute_import, division, print_function
+
+
+from .atan_filter import AtanFilter
+from .bound_filter import BoundFilter
+from .colour_filter import ColourFilter
+from .dct_filter import DCTFilter
+from .dht_filter import DHTFilter
+from .exp_filter import ExpFilter
+from .factor_filter import FactorFilter
+from .floor_filter import FloorFilter
+from .log_filter import LogFilter
+from .modulus_filter import ModulusFilter
+from .norm_filter import NormFilter
+from .otsu_filter import OtsuFilter
+from .sign_angle_diff_1d_filter import SignAngleDiff1DFilter
+from .sign_angle_diff_2d_filter import SignAngleDiff2DFilter
+from .sign_change_filter import SignChangeFilter
+
